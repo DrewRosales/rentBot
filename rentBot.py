02d6b8372a53db826fac4da2400b538e5eba379b
drew <drrosales99@gmail.com>
2020-05-21 14:44:42 -0400

    Adding initial files rentBot.py and sampleBot.py

---
+++ rentBot.py
@@ -0,0 +1,15 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
