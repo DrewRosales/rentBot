25541fb1f18f391d9ac6c63cd77b9128e5e8dae4
drew <drrosales99@gmail.com>
2020-05-21 22:54:15 -0400

    Fixed error in line 3

--- rentScraper.py
+++ rentScraper.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-def signIn:
+def signIn():
     URL = 'https://houghtonoffcampushousing.appfolio.com/connect/users/sign_in'
     page = requests.get(URL)
 
@@ -8,4 +8,3 @@ def signIn:
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
     }
 
-    
