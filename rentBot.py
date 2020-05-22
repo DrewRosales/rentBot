506ae60c159f84dd088ae1dad7c506ef6a93978e
drew <drrosales99@gmail.com>
2020-05-21 20:26:17 -0400

    Created rentScraper.py to gather information from the Tennant portal. Updated rentBot.py

--- rentBot.py
+++ rentBot.py
@@ -1,15 +1,39 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+token = "1145110613:AAFuDGcl_KXOEF_k4eYwjxIKXUzAvG25EEc"
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
+def start(update, context):
+    message = 'Gathering rent information...'
+    update.message.reply_text(message)
+
+def paid(update, context):
+    context.bot.
+
+def rentData():
+
+    
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 def main():
+    #Create an updater based on the bot's token
+    updater = Updater(token, use_context = True)
 
+    #Dispatcher to register handlers
+    dp = updater.dispacher
 
+    dp.add_handler(CommandHandler("start"), start)
+    dp.add_handler(CommandHandler("paid"), paid)
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle();
 
 if __name__ == '__main__':
     main()
