ea7a5c41d30ba6a806b478cb151dbe7588c5cb85
drew <drrosales99@gmail.com>
2020-05-21 22:53:25 -0400

    Added signIn()

--- rentScraper.py
+++ rentScraper.py
@@ -1,3 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
+def signIn:
+    URL = 'https://houghtonoffcampushousing.appfolio.com/connect/users/sign_in'
+    page = requests.get(URL)
 
-URL = ''
+    headers =  {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+    }
+
+    
