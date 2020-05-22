2144880caa06b49e68d680b054a1db20070b1ecb
drew <drrosales99@gmail.com>
2020-05-22 00:25:57 -0400

    Updated rentScraper, not logging in correctly

--- rentScraper.py
+++ rentScraper.py
@@ -1,10 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
-def signIn():
-    URL = 'https://houghtonoffcampushousing.appfolio.com/connect/users/sign_in'
-    page = requests.get(URL)
 
-    headers =  {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
-    }
 
+"""
+user[email]: drrosales99@gmail.com
+user[password]: speedkills99
+authenticity_token: o6X+GJr6j/vV8gIbenFdwjlGRoMDmqLLrtnyQ2v+fBWzqA2cEG2yVmTbyzmYKXDtzSK2ZiI3X4rsUDEDe0qWyg==
+
+"""
+payload = {
+    "user[email]"           : "drrosales99@gmail.com",
+    "user[password]"        : "speedkills99",
+    "authenticity_token"    : "o6X+GJr6j/vV8gIbenFdwjlGRoMDmqLLrtnyQ2v+fBWzqA2cEG2yVmTbyzmYKXDtzSK2ZiI3X4rsUDEDe0qWyg=="
+}
+
+#def scrap():
+s = requests.session()
+URL = 'https://houghtonoffcampushousing.appfolio.com/connect/users/sign_in'
+
+result = s.get(URL)
+#tree = html.fromstring(result.text)
+#token = list(set(tree.xpath("//input[@name='authenticity_token']/@value")))[0]
+
+
+result = s.post(
+        URL,
+        data = payload,
+        headers = dict(referer=URL)
+)
+
+print(result.url)
