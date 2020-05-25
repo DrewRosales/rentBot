0b1eaef01cf8c669ea1306dbe12919e8a7790cb4
drew <drrosales99@gmail.com>
2020-05-24 20:50:34 -0400

    Added comments and updated payload

--- rentScraper.py
+++ rentScraper.py
@@ -1,29 +1,37 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-"""
-user[email]: drrosales99@gmail.com
-user[password]: speedkills99
-"""
-payload = {
-    "user[email]"           : "drrosales99@gmail.com",
-    "user[password]"        : "speedkills99"
-}
 
-#def scrap():
+
+#Creates a session to store
 s = requests.session()
+
+#Login for tennant portal
 URL = 'https://houghtonoffcampushousing.appfolio.com/connect/users/sign_in'
 
+#get request at portal
 result = s.get(URL)
-#token = re.search(r'(?<=<meta name="csrf-token" content=)\w+', result.text)
+
+#convert portal to String
 str = result.text
+
+#Regular expressions to find the token beginning with the HTML tag
 token=re.search(r'(?<=<meta name="csrf-token" content=")\S+',str)
 token = token.group(0)
-#auth = re.findall(r'\"(\S+)\"',token)
+
+#Subtracts the last character \" from the String
 token = token[:-1]
 print(token)
-#print("\n\n\n\n")
-#print(str)
+
+
+payload = {
+    "utf8"                  :   "%25E2%259C%2593",
+    "authenticity_token"    :   token,
+    "user[email]"           :   "drrosales99@gmail.com",
+    "user[password]"        :   "speedkills99"
+    "commit"                :   "Log in"
+
+}
 
 result = s.post(
         URL,
