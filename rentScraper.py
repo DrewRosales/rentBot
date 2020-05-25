681bfccfafc4f98decb3b278fd19afcb4b5097eb
drew <drrosales99@gmail.com>
2020-05-24 21:13:38 -0400

    Successfully logged in and printed out next page's HTML

--- rentScraper.py
+++ rentScraper.py
@@ -1,8 +1,16 @@
 import requests, re
 from bs4 import BeautifulSoup
 
+headers = {
+    "content-type"                  :   "application/x-www-form-urlencoded",
+    "origin"                        :   "https://houghtonoffcampushousing.appfolio.com",
+    "referer"                       :   "https://houghtonoffcampushousing.appfolio.com/connect/users/sign_in",
+    "upgrade-insecure-requests"     :   "1",
+    "user-agent"                    :   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
 
 
+
+}
 #Creates a session to store
 s = requests.session()
 
@@ -28,14 +36,22 @@ payload = {
     "utf8"                  :   "%25E2%259C%2593",
     "authenticity_token"    :   token,
     "user[email]"           :   "drrosales99@gmail.com",
-    "user[password]"        :   "speedkills99"
+    "user[password]"        :   "speedkills99",
     "commit"                :   "Log in"
 
 }
 
 result = s.post(
         URL,
-        data = payload
+        headers =   headers,
+        data    =   payload,
+        cookies =   s.cookies
+        
 )
 
 print(result.url)
+
+
+result1 = s.get(result.url)
+str1 = result1.text
+print(str1)
